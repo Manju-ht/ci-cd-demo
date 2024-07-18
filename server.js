4c66c3e9703c97f6522c2ef77a8b091ce68ef4ad
Manjunath HT <141054261+Manju-ht@users.noreply.github.com>
2024-07-18 16:52:54 +0530

    Update server.js
    fsdfsd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 "use strict";
 var { app } = require('./app');
-const server = app.listen('8000', () => {
+const server = app.listen('8081', () => {
     console.log('server started');
 });
 module.exports = {
